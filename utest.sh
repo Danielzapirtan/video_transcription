6a1555c907314ffbb0215efbc100431b43a92c23
root <antoniudanielzapirtan@gmail.com>
2026-06-24 13:52:06 +0300

    Wed Jun 24 13:52:06 EEST 2026

---
+++ utest.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+VER=3
+
+if test -z $VIRTUAL_ENV; then
+	test -d venv || python$VER -m venv venv
+	source venv/bin/activate
+	export VIRTUAL_ENV
+fi
+apt install ffmpeg
+pip install -r requirements.txt
+python$VER app.py
+
